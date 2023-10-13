6275ecbd9d4f0817e49b31de6670456e34463965
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2023-10-13 09:31:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import BgColors from './components/BgColors';
+
+
+function App() {
+  return (
+    <>
+      <BgColors/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/BgColors.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BgColors() {
+    return (
+        <div>
+            <div class="container form-control my-2">
+                
+            </div>
+        </div>
+    )
+}
