b517778fef7c1881701d1f301ec7a778ae1cd56d
Saswat Mahapatra <92862073+logan1o1@users.noreply.github.com>
2023-10-14 22:36:38 +0530

    3rd task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import BgColors from './components/BgColors';
 
--- src/components/BgColors.js
+++ src/components/BgColors.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
+import './ColorPicker.css';
 
-export default function BgColors() {
+export default class BgColors extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          colors: [
+            'Red', 'Green', 'Blue', 'Yellow', 'Purple', 'Orange',
+            'Pink', 'Brown', 'Maroon', 'darkcyan', 'greenyellow',
+          ],
+          selectedColor: null,
+          displayColors: false,
+        };
+      }
+    
+      handleColorClick = (color) => {
+        this.setState({ selectedColor: color, displayColors: false });
+      }
+    
+      toggleColorDisplay = () => {
+        this.setState((prevState) => ({ displayColors: !prevState.displayColors }));
+      }
+    
+  render() {
+    const { selectedColor } = this.state;
+
     return (
-        <div>
-            <div class="container form-control my-2">
-                
+        <div className="color-picker">
+        <h2 className="color-picker-heading">Color Picker</h2>
+        <div className="color-picker-border">
+          <button
+            onClick={this.toggleColorDisplay}
+            style={{ backgroundColor: selectedColor ? selectedColor.toLowerCase() : 'white' }}
+          >
+            Pick a color
+          </button>
+          {this.state.displayColors && (
+            <div className="color-list">
+              {this.state.colors.map((color, index) => (
+                <div
+                  key={index}
+                  className="color-option"
+                  style={{ backgroundColor: color.toLowerCase() }}
+                  onClick={() => this.handleColorClick(color)}
+                >
+                  {color}
+                </div>
+              ))}
             </div>
+          )}
         </div>
+      </div>
     )
+  }
 }
+
+
